e63ecc4b522b9209e40752e9a26ee6cc423817ff
Astrono2 <jerescharovsky@gmail.com>
2021-08-04 16:27:04 -0300

    Added keyboard control
    Added outlines for tab selection, toggling switch with Enter and button press feedback

--- Documents/Popup/popup.js
+++ Documents/Popup/popup.js
@@ -3,3 +3,20 @@ document.getElementById('inject').onclick = async function() {
 	chrome.runtime.sendMessage({action: 'open_selection_context', tabId: tab.id});
 }
 
+document.getElementById('inject').onkeydown = function(self) {
+	if(self.key === "Enter") {
+		self.target.style.backgroundColor = 'var(--primary-color-semi-dark)';
+	}
+}
+
+document.getElementById('inject').onkeyup = function(self) {
+	if(self.key === "Enter") {
+		self.target.style.backgroundColor = '';
+	}
+}
+
+document.getElementById('block-all').onkeypress = function(self) {
+	if(self.key === "Enter") {
+		self.target.checked = !self.target.checked;
+	}
+}
