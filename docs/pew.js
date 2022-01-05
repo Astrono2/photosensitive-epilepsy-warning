ba380c99e2eaedfcf07d84ef50171a8120512320
Astrono2 <jerescharovsky@gmail.com>
2022-01-05 18:51:29 -0300

    Big update

--- docs/pew.js
+++ docs/pew.js
@@ -1,17 +1,29 @@
+var page = 'main';
+
 function orgs() {
+	page = 'orgs';
+	window.scrollTo(0,0);
 	let orgs = document.querySelector('.orgs');
 	let orgsPage = document.querySelector('.orgs-page');
 	orgs.style.zIndex = 200;
 	orgs.style.transition = 'transform 1s ease-in-out';
 	orgs.style.transform = 'translateY(100vh)';
 	orgsPage.style.transform = 'translateY(0)';
+	orgsPage.style.visibility = 'visible';
 	document.body.style.overflowY = 'hidden';
 	addState('#organizations');
+	setTimeout(() => {
+		let orgsLinks = document.querySelector('.orgs-links');
+		orgsLinks.classList.add('orgs-links-enabled');
+	}, 1000);
 }
 
 function unorgs() {
+	page = 'main';
 	let orgs = document.querySelector('.orgs');
 	let orgsPage = document.querySelector('.orgs-page');
+	let orgsLinks = document.querySelector('.orgs-links');
+	orgsLinks.classList.remove('orgs-links-enabled');
 	orgs.style.zIndex = 200;
 	orgs.style.transition = 'transform 1s ease-in-out';
 	orgs.style.transform = 'translateY(-15px)';
@@ -19,6 +31,8 @@ function unorgs() {
 	addState('');
 	setTimeout(() => {
 		let orgs = document.querySelector('.orgs');
+		let orgsPage = document.querySelector('.orgs-page');
+		orgsPage.style.visibility = '';
 		orgs.style.zIndex = '';
 		orgs.style.transition = '';
 		orgs.style.transform = '';
@@ -60,12 +74,14 @@ function info() {
 }
 
 function infoEpilepsy() {
+	page = 'info';
+	window.scrollTo(0,0);
 	let info = document.querySelector('.info-page');
 	let bubble = document.querySelector('.info-top-bubble');
 	info.style.visibility = 'visible';
 	info.style.opacity = 1.0;
-	info.style.backgroundColor = '#F2CCFF';
-	bubble.style.backgroundColor = '#663993';
+	info.style.backgroundColor = 'var(--pew-info-color)';
+	bubble.style.backgroundColor = 'var(--pew-secondary-color)';
 	bubble.textContent = 'Epilepsy';
 	let infoEp = document.querySelector('.info-ep');
 	let infoSe = document.querySelector('.info-se');
@@ -77,13 +93,16 @@ function infoEpilepsy() {
 }
 
 function infoSensory() {
+	page = 'info';
+	window.scrollTo(0,0);
 	let info = document.querySelector('.info-page');
 	let bubble = document.querySelector('.info-top-bubble');
 	info.style.visibility = 'visible';
 	info.style.opacity = 1.0;
-	info.style.backgroundColor = '#FFDFF6';
-	bubble.style.backgroundColor = '#F992DC';
+	info.style.backgroundColor = 'var(--pew-info-sensory-bg)';
+	bubble.style.backgroundColor = 'var(--pew-info-sensory)';
 	bubble.textContent = 'Sensory Disorders';
+	document.body.style.overflowY = 'hidden';
 	let infoEp = document.querySelector('.info-ep');
 	let infoSe = document.querySelector('.info-se');
 	let infoSu = document.querySelector('.info-su');
@@ -94,13 +113,16 @@ function infoSensory() {
 }
 
 function infoSupport() {
+	page = 'info';
+	window.scrollTo(0,0);
 	let info = document.querySelector('.info-page');
 	let bubble = document.querySelector('.info-top-bubble');
 	info.style.visibility = 'visible';
 	info.style.opacity = 1.0;
-	info.style.backgroundColor = '#C7F3CC';
-	bubble.style.backgroundColor = '#7DD185';
+	info.style.backgroundColor = 'var(--pew-info-support-bg)';
+	bubble.style.backgroundColor = 'var(--pew-info-support)';
 	bubble.innerHTML = 'Support<br>Groups';
+	document.body.style.overflowY = 'hidden';
 	let infoEp = document.querySelector('.info-ep');
 	let infoSe = document.querySelector('.info-se');
 	let infoSu = document.querySelector('.info-su');
@@ -111,6 +133,7 @@ function infoSupport() {
 }
 
 function uninfo() {
+	page = 'main';
 	let info = document.querySelector('.info-page');
 	let infoEp = document.querySelector('.info-ep');
 	let infoSe = document.querySelector('.info-se');
@@ -123,21 +146,26 @@ function uninfo() {
 	setTimeout(() => {
 		let info = document.querySelector('.info-page');
 		info.style.visibility = '';
+		document.body.style.overflowY = '';
 	}, 500);
 }
 
 function about() {
+	page = 'about';
+	window.scrollTo(0,0);
 	let about = document.querySelector('.about');
 	let aboutPage = document.querySelector('.about-page');
 	about.style.zIndex = 200;
 	about.style.transition = 'transform 1s ease-in-out';
 	about.style.transform = 'translateY(100vh)';
+	aboutPage.style.visibility = 'visible';
 	aboutPage.style.transform = 'translateY(0)';
 	document.body.style.overflowY = 'hidden';
 	addState('#about-us');
 }
 
 function unabout() {
+	page = 'main';
 	let about = document.querySelector('.about');
 	let aboutPage = document.querySelector('.about-page');
 	about.style.zIndex = 200;
@@ -147,6 +175,8 @@ function unabout() {
 	addState('');
 	setTimeout(() => {
 		let about = document.querySelector('.about');
+		let aboutPage = document.querySelector('.about-page');
+		aboutPage.style.visibility = '';
 		about.style.zIndex = '';
 		about.style.transition = '';
 		about.style.transform = '';
@@ -158,43 +188,67 @@ function addState(state) {
     let stateObj = { id: "100" };
       
     window.history.pushState(stateObj,
-             state, "/landing.html" + state);
+             state, "/index.html" + state);
 }
 
-// document.onmousemove = (event) => {
-// 	if(window.location.href.split('#')[1] !== undefined) {
-// 		return;
-// 	}
-// 	let eye = document.querySelector('object').getSVGDocument().getElementById('path833');
-// 	let iconBB = document.querySelector('object').getBoundingClientRect();
-// 	let eyeBB = eye.getBBox();
-// 	let [eyeX, eyeY] = [eyeBB.x + iconBB.x + eyeBB.width / 2, eyeBB.y + iconBB.y + eyeBB.height /2];
-// 	let [x, y] = [event.pageX - eyeX, event.pageY - eyeY];
-// 	let l = Math.sqrt(x * x + y * y);
-// 	x = x / (Math.log(l) * 20);
-// 	y = y / (Math.log(l) * 20);
+document.onmousemove = (event) => {
+	if(window.location.href.split('#')[1] !== undefined) {
+		return;
+	}
+
+	let eye = document.querySelector('object').getSVGDocument().getElementById('path833');
+	let iconBB = document.querySelector('object').getBoundingClientRect();
+	let eyeBB = eye.getBBox();
+	let [eyeX, eyeY] = [eyeBB.x + iconBB.x + eyeBB.width / 2, eyeBB.y + iconBB.y + eyeBB.height /2];
+	let [x, y] = [event.screenX - eyeX - window.screen.left, event.screenY - window.screen.top - eyeY];
+	let l = Math.sqrt(x * x + y * y);
+	x = x / (Math.log(l) * 20);
+	y = y / (Math.log(l) * 20);
 	
-// 	eye.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-// };
+	eye.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+};
+
+function back_to_main() {
+	switch(page) {
+		case 'orgs':
+			unorgs();
+			break;
+		case 'info':
+			uninfo();
+			break;
+		case 'about':
+			unabout();
+			break;
+	}
+}
 
 window.onload = () => {
 	switch(window.location.href.split('#')[1]) {
 		case undefined:
+			if(page != 'main') back_to_main();
 			break;
 		case 'organizations':
+			if(page != 'orgs') back_to_main();
 			orgs();
 			break;
 		case 'info-epilepsy':
+			if(page != 'info') back_to_main();
 			infoEpilepsy();
 			break;
 		case 'info-sensory':
+			if(page != 'info') back_to_main();
 			infoSensory();
 			break;
 		case 'info-support':
+			if(page != 'info') back_to_main();
 			infoSupport();
 			break;
 		case 'about-us':
+			if(page != 'about') back_to_main();
 			about();
 			break;
 	}
+	document.querySelector('object').getSVGDocument().onmousemove = document.onmousemove;
 }
+
+window.onpopstate = window.onload;
